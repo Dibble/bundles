b73369a2f06b613dfc2a2ccdb13ac06c3172ef1e
James Cotterell <officer.dibble1@gmail.com>
2021-03-25 17:12:31 +0000

    Add loading fallback

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import loadable from '@loadable/component'
 import { useState } from 'react'
+import Loading from './Loading'
 
 const Counter = loadable(() => import(/* webpackPrefetch: true */ './Counter'))
 const Todo = loadable(() => import(/* webpackPrefetch: true */ './Todo'))
@@ -15,8 +16,8 @@ const App = () => {
         <option value='counter'>counter</option>
         <option value='todo'>todo</option>
       </select>
-      {currentApp === 'counter' && <Counter />}
-      {currentApp === 'todo' && <Todo />}
+      {currentApp === 'counter' && <Counter fallback={<Loading />} />}
+      {currentApp === 'todo' && <Todo fallback={<Loading />} />}
     </>
   )
 }
