3c77c6f4f6025f78cf2eceb86737f348a627cb81
James Cotterell <officer.dibble1@gmail.com>
2021-03-26 16:49:31 +0000

    Code splitting, vendors and runtime chunks

--- webpack.config.js
+++ webpack.config.js
@@ -21,5 +21,17 @@ module.exports = {
   },
   experiments: {
     topLevelAwait: true
+  },
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all'
+        }
+      }
+    }
   }
 }
